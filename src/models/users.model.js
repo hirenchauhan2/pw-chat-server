88dc17fdbb996d3f2523ae01dcb08dd5879c1746
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2017-10-24 11:55:53 +0530

    Remove sequelize-json as db moved from MariaDB to Postgres.

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,7 +1,6 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
 const Sequelize = require('sequelize');
-const JSONField = require('sequelize-json');
 
 const DataTypes = Sequelize.DataTypes;
 
