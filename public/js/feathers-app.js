9c532fd2fff9f7fad4a8b84fab37a5029e93fa55
Hiren Chauhan <=>
2017-11-07 15:54:27 +0530

    Fixed URL issue.

--- public/js/feathers-app.js
+++ public/js/feathers-app.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef,no-unused-vars */
+const { protocol, host} = window.location
+const APP_URL = `${protocol}//${host}`
 
 const app = feathers()
   .configure(feathers.hooks())
-  .configure(feathers.rest('/').axios(window.axios));
+  .configure(feathers.rest(APP_URL).axios(window.axios));
