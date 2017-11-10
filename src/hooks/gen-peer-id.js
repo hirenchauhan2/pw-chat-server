3b95244c78542800375b95be3e781637cf442520
Hiren Chauhan <=>
2017-11-10 21:38:33 +0530

    Generate short and secure peerIds
    using `puid` package for generating 12 characterlong string id for
    peerId field. yeast seemed to be generating ids which conflicted with
    peer server.

--- src/hooks/gen-peer-id.js
+++ src/hooks/gen-peer-id.js
@@ -1,9 +1,10 @@
-const yeast = require('yeast');
+const PUID = require('puid');
+const puid = new PUID(true); // generate 12 character unique string id
 
 // eslint-disable-next-line no-unused-vars
 module.exports = function(options = {}) {
   return function genPeerId(hook) {
-    hook.data.peerId = yeast();
+    hook.data.peerId = puid.generate();
 
     return Promise.resolve(hook);
   };
