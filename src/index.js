33b70c3fa68ef2bcfaed9fb0c9aff05af6a5af9b
Hiren Chauhan <=>
2017-11-11 14:23:19 +0530

    add peerjs server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 /* eslint-disable no-console */
 const logger = require('winston');
+const Peer = require('peer');
+
 const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
+
+// PeerJS Server
+app.use('/peerjs', Peer.ExpressPeerServer(server, {
+  debug: true
+}));
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
