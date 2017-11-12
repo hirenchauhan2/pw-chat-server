a9559446d379aabd68dc7ab8995e718c6e51f96f
Hiren Chauhan <=>
2017-11-12 18:17:37 +0530

    added self signed ssl certs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-console */
 const logger = require('winston');
 const Peer = require('peer');
-
+const fs = require('fs');
 const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
 
 // PeerJS Server
 app.use('/peerjs', Peer.ExpressPeerServer(server, {
-  debug: true
+  debug: true,
+  ssl: {
+    key: fs.readFileSync('./config/sslcerts/key.pem'),
+    cert: fs.readFileSync('./config/sslcerts/cert.pem')
+  }
 }));
 
 process.on('unhandledRejection', (reason, p) =>
